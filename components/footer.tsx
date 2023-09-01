0ff1240f608c5776f8d4dfc30fae782a5de74a66
jamalvw <jvwill00@gmail.com>
2023-08-31 20:09:59 -0400

    Replace placeholders with store name from API

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,13 @@
-const Footer = () => {
+import getStore from "@/actions/get-store";
+
+const Footer = async () => {
+    const store = await getStore();
+    
     return (
         <footer className="bg-white border-t">
             <div className="mx-auto py-10">
                 <p className="text-center text-xs text-black">
-                    &copy; 2023 StoreName, Inc. All rights reserved.
+                    &copy; 2023 {store.name}
                 </p>
             </div>
         </footer>
